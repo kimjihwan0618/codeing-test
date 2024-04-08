f0b7baba38ada17169fe6ea14e26331422588637
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-04-08 15:54:44 +0900

    백준 문제번호 2563 풀이 추가

--- baekjoon/excute.js
+++ baekjoon/excute.js
@@ -1,41 +1,33 @@
-// // 로컬 환경과 제출 환경 구분
 const fs = require("fs");
 const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`
 const newline = process.platform == 'linux' ? '\n' : '\r\n';
-// // .txt 파일 파싱 
+
 let inputs = fs.readFileSync(filePath).toString().trim().split(newline);
-
-// 문제풀이
-let size = inputs.length;
-let sum = 0;
-let masjorCalcSum = 0;
-
-const subjectRating = {
-  "A+": 4.5,
-  "A0": 4.0,
-  "B+": 3.5,
-  "B0": 3.0,
-  "C+": 2.5,
-  "C0": 2.0,
-  "D+": 1.5,
-  "D0": 1.0,
-  "F": 0.0,
+const fullSquare = []
+for (let x = 1; x <= 100; x++) {
+  fullSquare.push([])
+  for (let y = 1; y <= 100; y++) {
+    fullSquare[x - 1].push([x, y, false])
+  }
 }
 
-inputs.forEach(subject => {
-  const obj = subject.split(" ");
-  if (!obj[2].includes("P")) {
-    sum += Number(obj[1]);
-  } else {
-    size -= 1;
+for (let i = 1; i < inputs.length; i++) {
+  const currentSquare = inputs[i].split(" ").map(Number);
+  const squareX = Number(currentSquare[0]);
+  const squareY = Number(currentSquare[1]);
+  for (let x = squareX; x < squareX + 10; x++) {
+    for (let y = squareY; y < squareY + 10; y++) {
+      fullSquare[x - 1][y - 1][2] = true;
+    }
   }
+}
+
+let cnt = 0;
+
+fullSquare.forEach((x, xIdx) => {
+  fullSquare[xIdx].forEach(cell => {
+    cell[2] ? cnt += 1 : "";
+  })
 })
 
-inputs.forEach(subject => {
-  const obj = subject.split(" ");
-  if (!obj[2].includes("P")) {
-    masjorCalcSum += (Number(obj[1]) * subjectRating[obj[2]])
-  }
-})
-
-console.log((masjorCalcSum / sum).toFixed(6))
+console.log(cnt)
