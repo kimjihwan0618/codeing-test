bf01161e8dfb6a945034cbe83a8343e497f2ab3d
kimJiHwan0618 <78426129+kimJiHwan0618@users.noreply.github.com>
2024-04-21 12:05:49 +0900

    백준 문제풀 2720 추

--- baekjoon/excute.js
+++ baekjoon/excute.js
@@ -1,33 +1,13 @@
-const fs = require("fs");
-const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`
-const newline = process.platform == 'linux' ? '\n' : '\r\n';
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
 
-let inputs = fs.readFileSync(filePath).toString().trim().split(newline);
-const fullSquare = []
-for (let x = 1; x <= 100; x++) {
-  fullSquare.push([])
-  for (let y = 1; y <= 100; y++) {
-    fullSquare[x - 1].push([x, y, false])
-  }
-}
+let [num, divider] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(' ')
+  .map((item) => Number(item));
 
-for (let i = 1; i < inputs.length; i++) {
-  const currentSquare = inputs[i].split(" ").map(Number);
-  const squareX = Number(currentSquare[0]);
-  const squareY = Number(currentSquare[1]);
-  for (let x = squareX; x < squareX + 10; x++) {
-    for (let y = squareY; y < squareY + 10; y++) {
-      fullSquare[x - 1][y - 1][2] = true;
-    }
-  }
-}
+let ans = num.toString(divider).toUpperCase();
 
-let cnt = 0;
-
-fullSquare.forEach((x, xIdx) => {
-  fullSquare[xIdx].forEach(cell => {
-    cell[2] ? cnt += 1 : "";
-  })
-})
-
-console.log(cnt)
+console.log(ans);
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-# 코딩 테스트 연습 
+# 코딩 테스트 연습
 
-여러 사이트의 코딩 테스트 문제풀이를 모아놓기 위한 저장소입니다. 
+여러 사이트의 코딩 테스트 문제풀이를 모아놓기 위한 저장소입니다.
 이저장소의 페이지 아래의 링크롤 통해 확인하실 수 있습니다.
 
 [https://kimjihwan0618.github.io/coding_test](https://kimjihwan0618.github.io/coding_test)
@@ -8,15 +8,25 @@
 ## 문제 풀이
 
 각 문제의 솔루션(solution)은 링크를 통해 확인할 수 있습니다.
+
 ### 프로그래머스
+
 Level : 1
+
 - [01. 달리기 경주](https://codepen.io/kimjihwan0618/pen/poBwOGR?editors=0010)
 - [02. 추억점수](https://codepen.io/kimjihwan0618/pen/NWmvrwz?editors=0010)
 - [03. 바탕화면 정리](https://codepen.io/kimjihwan0618/pen/NWmvrzL)
 
 ### 백준
+
 6단계
+
 - [01. 너의 평점은 (문제 번호 : 25206)](https://www.acmicpc.net/source/share/7d6497d790f34f87acd958a395b96915)
 
 7단계
+
 - [01. 색종이 (문제 번호 : 2563)](https://www.acmicpc.net/source/share/c674d0d678334692829b70b584c94e40)
+
+8단계
+
+- [01. 세탁소 사장 동혁 (문제 번호 : 2720)](https://www.acmicpc.net/source/share/db0ba2ee2ccd448ba84854a59b38f757)
